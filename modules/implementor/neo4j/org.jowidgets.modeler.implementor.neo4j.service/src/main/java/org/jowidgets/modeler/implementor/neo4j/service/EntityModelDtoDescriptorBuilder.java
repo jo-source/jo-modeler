7af4456563a0778357314c3e93c51a0255fcc83f
herr.grossmann <herr.grossmann@gmx.de>
2012-10-01 05:59:43 +0000

    added type property , i18n not yet finished

--- modules/implementor/neo4j/org.jowidgets.modeler.implementor.neo4j.service/src/main/java/org/jowidgets/modeler/implementor/neo4j/service/EntityModelDtoDescriptorBuilder.java
+++ modules/implementor/neo4j/org.jowidgets.modeler.implementor.neo4j.service/src/main/java/org/jowidgets/modeler/implementor/neo4j/service/EntityModelDtoDescriptorBuilder.java
@@ -42,11 +42,12 @@ import org.jowidgets.cap.common.api.bean.IProperty;
 import org.jowidgets.cap.common.api.bean.IPropertyBuilder;
 import org.jowidgets.cap.common.api.sort.ISort;
 import org.jowidgets.cap.common.api.validation.IBeanValidator;
+import org.jowidgets.cap.service.neo4j.api.GraphDBConfig;
 import org.jowidgets.i18n.api.IMessage;
 import org.jowidgets.modeler.common.i18n.entity.ModelerEntityMessages;
+import org.jowidgets.modeler.service.persistence.bean.AbstractPropertyModel;
 import org.jowidgets.modeler.service.persistence.bean.EntityModel;
 import org.jowidgets.modeler.service.persistence.bean.EntityPropertyModel;
-import org.jowidgets.modeler.service.persistence.bean.AbstractPropertyModel;
 import org.jowidgets.util.Assert;
 
 final class EntityModelDtoDescriptorBuilder {
@@ -79,6 +80,7 @@ final class EntityModelDtoDescriptorBuilder {
 			result.add(createProperty(property));
 		}
 		result.add(createVersionProperty());
+		result.add(createBeanTypeProperty());
 		return result;
 	}
 
@@ -135,6 +137,17 @@ final class EntityModelDtoDescriptorBuilder {
 		final IBeanPropertyBuilder builder = CapCommonToolkit.beanPropertyBuilder(IBean.class, IBean.VERSION_PROPERTY);
 		builder.setLabel(getMessage("version.label"));
 		builder.setDescription(getMessage("version.description"));
+		builder.setVisible(false);
+		return builder.build();
+	}
+
+	private static IProperty createBeanTypeProperty() {
+		final IPropertyBuilder builder = CapCommonToolkit.propertyBuilder();
+		builder.setName(GraphDBConfig.getBeanTypePropertyName());
+		builder.setValueType(String.class);
+		//TODO MG i18n
+		builder.setLabel("Type");
+		builder.setDescription("The type of the dataset");
 		builder.setVisible(false);
 		return builder.build();
 	}
